cbc948637c80c04b6fa77a14fbb03af5ff444baf
matheus-zz <rodrigues.rocha.matheus@escola.pr.gov.br>
2025-06-15 18:51:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    '1',
+    'Qual é o maior continente do mundo?',
+    'Ásia'
+)
+
+criaCartao(
+    '2',
+    ' Qual é o maior país do mundo em área?',
+    'Rússia'
+)
+
+criaCartao(
+    '3',
+    'Qual é o rio mais longo do mundo?',
+    'Nilo'
+)
+
+criaCartao(
+    '4',
+    'Qual é a montanha mais alta do mundo?',
+    'Everest'
+)
